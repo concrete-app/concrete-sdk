39bc63008f810b82c3fc5e396252efd8493a0e7f
Marco Furrer <marcofurrer0@gmail.com>
2026-04-08 21:31:08 +0200

    fertig

--- sdks/typescript/api/types/index.ts
+++ sdks/typescript/api/types/index.ts
@@ -1,5 +1,6 @@
 export * from "./HealthStatus.js";
 export * from "./Product.js";
 export * from "./ProductConfig.js";
+export * from "./Stripe.js";
 export * from "./UploadResponse.js";
 export * from "./VectorUpdateResponse.js";
